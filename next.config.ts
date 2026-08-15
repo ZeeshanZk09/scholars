062db47270f303f60aba0c81c57f0c93d3ef0fe9
ZeeshanZk09 <mzeeshankhan0988@gmail.com>
2026-08-15 14:12:11 +0500

    chore: comment out script-src Content-Security-Policy directive in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -23,7 +23,7 @@ const securityHeaders = [
     key: "Content-Security-Policy",
     value: [
       "default-src 'self'",
-      `script-src 'self' 'unsafe-inline'${isProduction ? "" : " 'unsafe-eval'"}`,
+      // `script-src 'self' 'unsafe-inline'${isProduction ? "" : " 'unsafe-eval'"}`,
       "style-src 'self' 'unsafe-inline'",
       "img-src 'self' data: blob: https://placehold.co",
       "font-src 'self' data:",
